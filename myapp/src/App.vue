1631897615043b67383e96a88b0be04b5669da96
安藤美紗子 <otokomaetohu@gmail.com>
2020-10-04 17:58:27 +0900

    Vue.js-Bootstrap4

---
+++ myapp/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+    <button class="btn btn-primary">bootstrap</button>
+    <b-button variant="primary">bootstrap</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
